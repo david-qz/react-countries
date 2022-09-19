5e4eb1b1dd9c2a10226383ab03e043037edbd7b5
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-19 16:51:08 -0700

    Add filtering

---
+++ src/components/FilterControls/FilterControls.js
@@ -0,0 +1,29 @@
+import './FilterControls.css';
+
+export default function FilterControls({
+  continentFilter, setContinentFilter,
+  textFilter, setTextFilter
+}) {
+  return (
+    <div className='FilterControls'>
+      <input
+        onChange={(e) => setTextFilter(e.target.value)}
+        value={textFilter}
+        placeholder="country name"
+      ></input>
+      <select
+        onChange={(e) => setContinentFilter(e.target.value)}
+        value={continentFilter}
+      >
+        <option value="all">All</option>
+        <option value="Africa">Africa</option>
+        <option value="Antarctica">Antarctica</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="North America">North America</option>
+        <option value="South America">South America</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+    </div>
+  );
+}
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,36 @@
 import './Main.css';
 import Header from '../layout/Header/Header';
 import Footer from '../layout/Footer/Footer';
+import FilterControls from '../FilterControls/FilterControls';
 import CountryList from '../CountryList/CountryList';
 import useCountries from '../../hooks/useCountries';
+import { useState } from 'react';
 
 export default function Main() {
   const countries = useCountries();
 
+  const [continentFilter, setContinentFilter] = useState('all');
+  const [textFilter, setTextFilter] = useState('');
+
+  let filteredCountries = countries.filter(country => {
+    if (continentFilter === 'all') return true;
+    return country.continent === continentFilter;
+  });
+
+  filteredCountries = filteredCountries.filter(country => {
+    return country.name.match(new RegExp(`.*${textFilter}.*`, 'i'));
+  });
+
   return (
     <div className='Main'>
       <Header />
-      <CountryList countries={countries} />
+      <main>
+        <FilterControls
+          continentFilter={continentFilter} setContinentFilter={setContinentFilter}
+          textFilter={textFilter} setTextFilter={setTextFilter}
+        />
+        <CountryList countries={filteredCountries} />
+      </main>
       <Footer />
     </div>
   );
