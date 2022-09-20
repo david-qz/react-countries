405ce2841a3894b73d4c6734b56e281065a9778b
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-19 20:04:31 -0700

    Sort countries by name
    It would be cool to offer other sorting options, but I don't see
    anything obvious in the data to sort by. The innate sort of the database
    doesn't seem to have any obvious pattern. It's roughly alphabetical, but
    not completely.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -25,6 +25,11 @@ export default function Main() {
   const [continentFilter, setContinentFilter] = useState('all');
 
   const filteredCountries = filterCountries(countries, textFilter, continentFilter);
+  filteredCountries.sort((a, b) => {
+    if (a.name > b.name) return 1;
+    if (a.name < b.name) return -1;
+    return 0;
+  });
 
   return (
     <main>
