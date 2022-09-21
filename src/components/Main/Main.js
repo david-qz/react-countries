83b2263a3a8eb64b2973f2215577eb2436d291a5
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-20 17:12:08 -0700

    Move filtering and sorting into and custom hook
    I didn't want to shove this all into the existing useCountries hook, but
    it looks like React lets you compose custom hooks. Neat.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,43 +2,15 @@ import './Main.css';
 import FilterControls from '../FilterControls/FilterControls';
 import CountryList from '../CountryList/CountryList';
 import LoadingIcon from '../LoadingIcon/LoadingIcon';
-import useCountries from '../../hooks/useCountries';
-import { useState } from 'react';
-
-function filterCountries(countries, textFilter, continentFilter) {
-  return countries.filter(country => {
-    const { continent, name } = country;
-
-    if (!(continentFilter === 'all' || continent === continentFilter))
-      return false;
-
-    if (!(name.match(new RegExp(`.*${textFilter}.*`, 'i'))))
-      return false;
-
-    return true;
-  });
-}
-
-function sortCountriesByName(countries, sortIsAscending) {
-  const sortSign = sortIsAscending ? -1 : 1;
-
-  countries = [...countries];
-  countries.sort((a, b) => {
-    return a.name.localeCompare(b.name) * sortSign;
-  });
-
-  return countries;
-}
+import useFilteredSortedCountries from '../../hooks/useFilteredSortedCountries';
 
 export default function Main() {
-  const [countries, loading, error] = useCountries();
-
-  const [textFilter, setTextFilter] = useState('');
-  const [continentFilter, setContinentFilter] = useState('all');
-  const [sortIsAscending, setSortIsAscending] = useState(false);
-
-  const filteredCountries = filterCountries(countries, textFilter, continentFilter);
-  const filteredAndSortedCountries = sortCountriesByName(filteredCountries, sortIsAscending);
+  const {
+    filteredAndSortedCountries, loading, error,
+    textFilter, setTextFilter,
+    continentFilter, setContinentFilter,
+    sortIsAscending, setSortIsAscending
+  } = useFilteredSortedCountries();
 
   return (
     <main>
